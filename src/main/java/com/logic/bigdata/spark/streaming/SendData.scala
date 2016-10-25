0df13f05b23a06f95b4400e2518a851c0be57753
LogicStarDust <657496651@qq.com>
2016-10-25 13:48:28 +0800

    首次提交到git本地

---
+++ src/main/java/com/logic/bigdata/spark/streaming/SendData.scala
@@ -0,0 +1,35 @@
+package com.logic.bigdata.spark.streaming
+
+import java.net.ServerSocket
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+/**
+  * @author Wang Guodong wangguodong@richinfo.cn
+  */
+object SendData {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("sendData")
+    val sparkContext = new SparkContext(sparkConf)
+    val data = sparkContext.textFile("hdfs://192.168.1.101:8020/test/dayLog.out")
+    val lines = data.collect()
+    val socketServer=new ServerSocket(6666)
+    while(true){
+      val socket=socketServer.accept()
+      val out=socket.getOutputStream
+      while (true){
+        lines.foreach(line=>{
+          Thread.sleep(1000*3)
+          println(line)
+          out.write((line+"\n").getBytes)
+          out.flush()
+        })
+      }
+      out.close()
+    }
+
+  }
+}
+
---
+++ src/main/java/com/logic/bigdata/spark/streaming/Streaming.scala
@@ -0,0 +1,25 @@
+package com.logic.bigdata.spark.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * @author Wang Guodong wangguodong@richinfo.cn
+  *
+  *
+  */
+object Streaming {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf=new SparkConf().setAppName("streaming")
+    val streamingContext=new StreamingContext(sparkConf,Seconds(5))
+
+    val lines=streamingContext.socketTextStream("192.168.3.3",6666)
+
+    lines.print()
+
+    streamingContext.start()
+    streamingContext.awaitTermination()
+
+  }
+}
