d0e91b6857e7527fdbaca59363bcc12116ec5401
LogicStarDust <657496651@qq.com>
2016-10-25 14:25:00 +0800

    中途

--- src/main/java/com/logic/bigdata/spark/streaming/com/logic/scala/akka/AkkaTest.scala
+++ src/main/java/com/logic/bigdata/spark/streaming/com/logic/scala/akka/AkkaTest.scala
@@ -1,8 +1,23 @@
 package com.logic.bigdata.spark.streaming.com.logic.scala.akka
 
+import akka.actor.{Actor, ActorSystem, Props}
+
 /**
   * @author Wang Guodong wangguodong@richinfo.cn
   */
-object AkkaTest {
+object AkkaTest extends App {
+  val system = ActorSystem("actor-demo-scala")
+  val hello = {
+    system.actorOf(Props[Hello])
+  }
+  hello.!("Bob")
+  Thread.sleep(1000)
+  system.shutdown()
+
+  class Hello extends Actor {
+    override def receive: Receive = {
+      case name: String => println(s"Hello $name")
+    }
+  }
 
 }
