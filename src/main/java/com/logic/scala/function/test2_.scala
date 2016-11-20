7789f7027e63692f82d5e23c39079db9132fcecf
logic <657496651@qq.com>
2016-11-20 15:09:13 +0800

    test2_

--- src/main/java/com/logic/scala/function/test2_.scala
+++ src/main/java/com/logic/scala/function/test2_.scala
@@ -3,7 +3,7 @@ package com.logic.scala.function
 /**
   * Created by logic on 2016/11/20.
   */
-object test2_3 {
+object test2_ {
   def main(args: Array[String]): Unit = {
     def add(one:Int,two:Long):BigDecimal={
       BigDecimal(one)+BigDecimal(two)
@@ -16,5 +16,9 @@ object test2_3 {
   def curry[A, B, C](f: (A, B) => C): A => (B => C)=
     (a:A)=>{
       f(a,_)
+    }
+  def uncurry[A,B,C](f:A=>B=>C):(A,B)=>C=
+    (a:A,b:B)=>{
+      f(a)(b)
     }
 }
