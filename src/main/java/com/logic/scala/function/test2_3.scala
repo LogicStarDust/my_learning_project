cb1522bc26cce5e982209960eb2ac23b20dd54ac
logic <657496651@qq.com>
2016-11-20 14:38:43 +0800

    test2.3

---
+++ src/main/java/com/logic/scala/function/test2_3.scala
@@ -0,0 +1,20 @@
+package com.logic.scala.function
+
+/**
+  * Created by logic on 2016/11/20.
+  */
+object test2_3 {
+  def main(args: Array[String]): Unit = {
+    def add(one:Int,two:Long):BigDecimal={
+      BigDecimal(one)+BigDecimal(two)
+    }
+
+    val cu=curry(add)
+    val value=cu(10)
+    println(value(10))
+  }
+  def curry[A, B, C](f: (A, B) => C): A => (B => C)=
+    (a:A)=>{
+    f(a,_)
+  }
+}
