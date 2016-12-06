6cc07a4b58137012b27a1a0f2cd2d09cfa755db0
LogicStarDust <657496651@qq.com>
2016-12-06 10:16:54 +0800

    scala函数式编程习题3单元16-23完成

--- src/main/scala/com/logic/functional/programming/chapter3/Test3_16until23.scala
+++ src/main/scala/com/logic/functional/programming/chapter3/Test3_16until23.scala
@@ -30,8 +30,27 @@ object Test3_16until23 {
     Test3_7until15.appendMLS(map(ml)(f))
   }
   //3.21
-  def filterByFM[A](ml:MyList[A])(f:A=>Boolean):MyList[A]={
-    flatMap(ml)
+  def filterByFM[A](ml:MyList[A])(f:A=>Boolean):MyList[A]= {
+    flatMap(ml)(x => {
+      if (f(x)) MyList(x)
+      else MyNil
+    })
+}
+  //3.22
+  def twoMyListFoldAdd(ml1:MyList[Int],ml2:MyList[Int]):MyList[Int]=ml1 match {
+    case MyNil=>MyNil
+    case ml if ml2==MyNil=>ml
+    case Cons(h,t)=>Cons(h+head(ml2),twoMyListFoldAdd(t,Test3_1until6.tail(ml2)))
+  }
+  def head[A](ml:MyList[A]):A=ml match {
+    case Cons(h,_)=>h
+  }
+
+  //3.23
+  def zipWith[A,B](ml1:MyList[A],ml2:MyList[B])(f:(A,B)=>A):MyList[A]=ml1 match {
+    case MyNil=>MyNil
+    case ml if ml2==MyNil=>ml
+    case Cons(h,t)=>Cons(f(h,head(ml2)),zipWith(t,Test3_1until6.tail(ml2))(f))
   }
   def main(args: Array[String]): Unit = {
    val ml=MyList(1,2,3,4,5)
@@ -45,7 +64,12 @@ object Test3_16until23 {
     println(filter(ml)(_%2==0))
     //3.20
     println(flatMap(MyList(1,2,3))(x=>MyList(x,x)))
-    //2.21
-
+    //3.21
+    println(filterByFM(ml)(_%2==0))
+    //3.22
+    println(twoMyListFoldAdd(ml,ml))
+    //3.23
+    println(zipWith(ml,ml)(_+_))
+    println(zipWith(ml,ml)(_*_))
   }
 }
