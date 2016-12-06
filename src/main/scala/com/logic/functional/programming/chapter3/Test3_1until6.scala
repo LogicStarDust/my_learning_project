77011300b98a3fea823182e164469b83757251fc
LogicStarDust <657496651@qq.com>
2016-12-06 10:19:03 +0800

    scala函数式编程习题3.1到6加注释

--- src/main/scala/com/logic/functional/programming/chapter3/Test3_1until6.scala
+++ src/main/scala/com/logic/functional/programming/chapter3/Test3_1until6.scala
@@ -13,7 +13,7 @@ object Test3_1until6 {
     val ml4=MyList(MyNil)
     println(dropWhile(ml)(x=>x<3))
   }
-
+  //3.1
   def threePointOne: Int = {
     MyList(1, 2, 3, 4, 5) match {
       case Cons(x, Cons(2, Cons(4, _))) => x
@@ -23,28 +23,29 @@ object Test3_1until6 {
       case _ => 101
     }
   }
-
+  //3.2
   def tail[A](ml: MyList[A]): MyList[A] = ml match {
     case MyNil => MyNil
     case Cons(_, t) => t
   }
-
+  //3.3
   def setHead[A](head: A)(ml: MyList[A]): MyList[A] = ml match {
     case MyNil => Cons(head, MyNil)
     case Cons(_, t) => Cons(head, t)
   }
-
+  //3.4
   def drop[A](n: Int)(ml: MyList[A]): MyList[A] = n match {
     case 0 => ml
     case 1 => tail(ml)
     case x => drop(x - 1)(tail(ml))
   }
-
+  //3.5
   def dropWhile[A](ml: MyList[A])(f: A => Boolean): MyList[A] = ml match {
     case MyNil => MyNil
     case Cons(h, t) if f(h) => dropWhile(t)(f)
     case x=>x
   }
+  //3.6
   def init[A](ml:MyList[A]):A=ml match {
 //    case MyNil=>??
     case Cons(h,MyNil)=>h
