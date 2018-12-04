1307620bd11fefe8384a7bf1edc4b09ce7ef0b3f
unknown <guodong5@staff.sina.com.cn>
2018-12-04 17:55:55 +0800

    1

--- src/main/python/nlp/nbayes_test.py
+++ src/main/python/nlp/nbayes_test.py
@@ -10,3 +10,8 @@ nb=NBays()
 nb.train_set(dataSet,listClasses)
 nb.map2vocab(dataSet[0])
 print nb.predict(nb.testset)
+
+# 1.通过训练集计算每个词属于每个分类的概率
+# 2.通过训练集计算每个类别的概率
+# 3.对于测试文本，假设属于某个类别的时候，计算文本所有词属于本类别的概率，再乘以本类别的概率
+# 4.取第3步概率最高的那个分类
